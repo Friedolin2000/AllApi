edbce8610cbbe83ddfdbd194d65bd6b54b62f5c9
IronException <mail-fuer-friedolin@web.de>
2019-01-25 11:45:26 +0000

    Add files via upload

---
+++ Place.java
@@ -0,0 +1,39 @@
+
+
+
+
+public class Place {
+  
+  String place;
+  
+  public Place(){
+    
+  }
+  
+  public Place(String path){
+    
+    
+  }
+  
+  public String[] getSubPlaces(){
+    return null;
+  }
+  
+  public String[] getAllSubPlaces(){
+    return null;
+  }
+    
+  public void becomePlace(String subPlace){
+    
+  }  
+  
+  
+  public int distance(Place p){
+    
+    return 0;
+  }
+  
+  
+  
+  
+}
